934e5569cdb5f51eee53f02fb47db1ef719d65f2
Pavlos Vinieratos <pvinis@gmail.com>
2021-03-07 18:43:51 +0000

    token

--- index.js
+++ index.js
@@ -9,30 +9,37 @@ require('dotenv').config()
 
 
 const main = async () => {
-// try {
 	const ZONE = core.getInput('zone')
 //   const time = (new Date()).toTimeString();
 //   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify(github.context.payload, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
+
+
+	const TOKEN = process.env.CLOUDFLARE_TOKEN
+	if (TOKEN === undefined) {
+		console.log("Cloudflare token not found. Make sure to add one in GitHub environments.")
+		core.setFailed("Cloudflare token not found.")
+		exit(-1)
+	}
+
+	const cf = new Cloudflare({
+		token: TOKEN
+	})
 
 
 	const rawText = fs.readFileSync("./DNS-RECORDS.hjson").toString()
 	const config = HJSON.parse(rawText)
-	console.log(HJSON.stringify(config))
 
 
 	// Find the right zone
 	const zones = await cf.zones.browse()
 	const theZones = zones.result.filter(zone => zone.name === ZONE).map(zone => zone.id)
 	if (theZones.length === 0) {
-		console.log(`No zones found with name: ${config.zone}`)
-		console.log("Make sure you have it right in DNS-RECORDS.hjson")
-		core.setFailed("Zone not found")
+		console.log(`No zones found with name: ${config.zone}.`)
+		console.log("Make sure you have it right in DNS-RECORDS.hjson.")
+		core.setFailed("Zone not found.")
 		exit(-1)
 	}
 
